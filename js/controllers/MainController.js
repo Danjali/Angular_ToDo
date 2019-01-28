79cefa7286ec08dd3954cf4477e48940e270b161
Danjali <anjalideoasarkar@gmail.com>
2019-01-28 15:17:28 +0530

    added new features in ToDoList

--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -1,31 +1,87 @@
-app.controller('MainController', ['$scope', ($scope) => {
-  $scope.title = 'ToDo List';
-  $scope.todoList =[];
+app.controller('MainController', ['$scope', function ($scope) {
+   $scope.title = 'ToDo List';
+   $scope.todoList = [];
+   $scope.buttonValue = 'Add';
 
-  $scope.addToList = function() {
-    if($scope.todoInput) {
-       $scope.todoList.push({ todoText: $scope.todoInput, done: false });
-       $scope.todoInput = '';
-       // adding property 'id' to each object of the array
-       $scope.todoList.forEach((item, i) =>  item.id = i + 1; );
-     }
+   $scope.addToList = function () {
+      if ($scope.buttonValue === 'Add') {
+         if ($scope.todoInput && !$scope.todoList.length) {
+            $scope.todoList.push({
+               todoText: $scope.todoInput,
+               done: false
+            });
+            $scope.todoInput = '';
+         } else if (checkDuplicate($scope.todoList)) {
+            alert('Duplicate Task');
+         } else if ($scope.todoInput) {
+            $scope.todoList.push({
+               todoText: $scope.todoInput,
+               done: false
+            });
+            $scope.todoInput = '';
+         }
+      } else {
+         if (checkDuplicate($scope.todoList)) {
+            alert('Duplicate Task');
+         } else if ($scope.todoInput) {
+            $scope.todoList[$scope.editedId] = {
+               todoText: $scope.todoInput,
+               done: false
+            };
+            $scope.todoInput = '';
+            $scope.buttonValue = 'Add';
+         } else {
+            alert('Task is empty. Add Task!!')
+         }
+      }
+      sortTheList($scope.todoList);
    };
 
-   $scope.removeFromList = function(id) {
-       let oldList = $scope.todoList;
-       $scope.todoList = [];
-       // filter items from the list
-       $scope.todoList = oldList.filter(item => item.id !== id; );
+   $scope.removeFromList = function (id) {
+      let oldList = $scope.todoList;
+      if (id !== -1) {
+         // remove items from the list
+         oldList.splice(id, 1);
+      }
+      $scope.todoList = oldList; // re-assign the filtered list
+      $scope.buttonValue = 'Add';
    };
 
-  $scope.markAsComplete = function() {
+   $scope.markAsComplete = function () {
       let list = $scope.todoList;
-      $scope.completedList = list.filter(item => { item.done === true; ); // adding property 'flag' to each object of the array to show
-        $scope.completedList.forEach(item =>  item.flag = true; );
-      };
+      $scope.completedList = list.filter(item => item.done === true);
+      // adding property 'flag' to each object of the array to show
+      $scope.completedList.map(item => item.flag = true);
+      sortTheList($scope.todoList);
+   };
 
-   $scope.removeAll = function() { // remove all items from list
-     $scope.todoList = [];
+   $scope.removeAll = function () {
+      $scope.todoList = []; // remove all items from list
+      $scope.buttonValue = 'Add';
+   };
+
+   $scope.editTask = function (id) {
+      $scope.buttonValue = 'Update';
+      $scope.todoList.forEach((item, key) => {
+         if (key === id) {
+            $scope.todoInput = $scope.todoList[key].todoText;
+            $scope.editedId = id;
+         }
+      })
+   };
+
+   sortTheList = function (updatedList) { //completed tasks will be moved to end of the list
+      $scope.todoList = updatedList.sort((a, b) => a.done - b.done);
+   };
+
+   checkDuplicate = function (list) {
+      let validateFlag;
+      list.forEach(item => {
+         if (item.todoText === $scope.todoInput) {
+            validateFlag = true;
+         }
+      });
+      return validateFlag;
    };
 
 }]);
